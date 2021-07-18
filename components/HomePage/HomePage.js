9037f2d6805f8c15839308ed447dfe58ff5ca96d
starnizar <nizarincorp@gmail.com>
2021-07-18 18:57:27 +0300

    Фильтры коптеров --- done

--- components/HomePage/HomePage.js
+++ components/HomePage/HomePage.js
@@ -5,24 +5,47 @@ import { useNavigation } from '@react-navigation/core';
 const Home = () => {
 
     const copters = [
-        {id:1, name:'DJI Air 2S', price:1424, rate:4.2, img:require('../../assets/copters/Copter1.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
-        {id:2, name:'DJI Mavic Mini', price:990.90, rate:4.5, img:require('../../assets/copters/Copter2.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
-        {id:3, name:'DJI’s Matrice 200', price:2780.30, rate:3.8, img:require('../../assets/copters/Copter3.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
+        {id:1, name:'DJI Air 2S', price:'1424', rate:4.2, speed: 19, img:require('../../assets/copters/Copter1.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
+        {id:2, name:'DJI Mavic Mini', price:'990.90', rate:4.5, speed: 16, img:require('../../assets/copters/Copter2.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
+        {id:3, name:'DJI’s Matrice 200', price:'2780.30', rate:3.8, speed: 23, img:require('../../assets/copters/Copter3.png'), description:'The Mavic 2 offers iconic Hasselblad image quality on Pro and a high-performance zoom lens on Zoom.'},
     ];
     const [showCopters, setShowCopters] = useState([...copters]);
+    const [copterFilter, setCopterFilter] = useState('All Quadcopters')
     const navigation = useNavigation();
 
     const showAll = () => {
+        setCopterFilter('All Quadcopters')
         setShowCopters([...copters])
     };
     const showCheap = () => {
-
+        setCopterFilter('Cheap Quadcopters')
+        let cheapest = copters[0];
+        for(let i=1; i<copters.length;i++){
+            if(+cheapest.price > +copters[i].price){
+                cheapest = copters[i];
+            };
+        };
+        setShowCopters([cheapest])
     };
     const showBest = () => {
-
+        setCopterFilter('Best Quadcopters')
+        let theBest = copters[0];
+        for(let i=1; i<copters.length;i++){
+            if(theBest.rate < copters[i].rate){
+                theBest = copters[i];
+            };
+        };
+        setShowCopters([theBest])
     };
     const showFast = () => {
-        
+        setCopterFilter('Fast Quadcopters')
+        let fastest = copters[0];
+        for(let i=1; i<copters.length;i++){
+            if(fastest.speed < +copters[i].speed){
+                fastest = copters[i];
+            };
+        };
+        setShowCopters([fastest])
     };
 
     return <View style={styles.container}>
@@ -47,21 +70,29 @@ const Home = () => {
                     />
                 </View>
                 <View>
-                    <Pressable>
+                    <Pressable
+                        onPress={showAll}
+                    >
                         <Text>All</Text>
                     </Pressable>
-                    <Pressable>
+                    <Pressable
+                        onPress={showCheap}
+                    >
                         <Text>Cheap</Text>
                     </Pressable>
-                    <Pressable>
+                    <Pressable
+                        onPress={showBest}
+                    >
                         <Text>Best</Text>
                     </Pressable>
-                    <Pressable>
+                    <Pressable
+                        onPress={showFast}
+                    >
                         <Text>Fast</Text>
                     </Pressable>
                 </View>
                 <View>
-                    <Text>AllCopters</Text>
+                    <Text>{copterFilter}</Text>
                     <ScrollView
                         horizontal={true}
                     >
@@ -83,7 +114,7 @@ const Home = () => {
                                         >
                                             <Text>{`${copter.price} $`}</Text>
                                             <View
-                                                style={{flexDirection:'row'}}
+                                                style={{flexDirection:'row', alignItems:'center'}}
                                             >
                                                 <Image
                                                     style={{width:11.65, height:11.08}}
@@ -107,7 +138,7 @@ const styles = StyleSheet.create({
     container:{
         width: '100%',
         height: '100%',
-        backgroundColor:'#E7E7E7'
+        backgroundColor:'#F8F8F8'
     },
     content:{
         width: '100%',
